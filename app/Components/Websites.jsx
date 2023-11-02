191f39b264f98d540accbdf6706429822238cee5
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-02 23:18:21 +0530

    modified:   app/Components/Websites.jsx

--- app/Components/Websites.jsx
+++ app/Components/Websites.jsx
@@ -17,7 +17,7 @@ export default function WebLinks(){
             <Link className="ind-web" href='https://6543d93bd052e22381dc3020--vermillion-kitten-ebe710.netlify.app/'target='_blank'><img src={burger.src} /></Link>
             <Link className="ind-web" href='https://6543dafbe0e90d1e3276f639--dainty-pika-ccf3b3.netlify.app/' target="_blank"><img src={pass.src} /></Link>
             <Link className="ind-web" href='https://6543dc07c8cb3a2b2ad7b5a0--dreamy-licorice-4a4e96.netlify.app/' target="_blank"><img src={scoreboard.src} /></Link>
-            <Link className="ind-web bg-gray-100" href='https://transcendent-sopapillas-e2eaee.netlify.app/' target="_blank"><img src={converter.src} /></Link>
+            <Link className="ind-web bg-gray-100" href='https://6543dfdb8452f2347c61cbcb--deluxe-cranachan-f3c317.netlify.app/' target="_blank"><img src={converter.src} /></Link>
             <Link className="ind-web" href='https://timely-lollipop-fb855f.netlify.app/' target="_blank"><img src={chats.src} /></Link>
             <Link className="ind-web" href='https://dainty-blini-640110.netlify.app/login' target="_blank"><img src={funding.src} /></Link>
             <Link className="ind-web" href='https://gtdlast-git-master-abelkjohns-projects.vercel.app' target="_blank"><img src={gtd.src} /></Link>
