30272054e7e4a12bc30a0388fb56819c7c40ba85
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-02 09:26:20 +0530

    new file:   app/Components/Footer.jsx 	new file:   app/Components/Header.jsx 	new file:   app/Components/Websites.jsx 	modified:   app/globals.css 	deleted:    app/page.js 	new file:   app/page.jsx

---
+++ app/Components/Websites.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Link from "next/link";
+
+export default function WebLinks(){
+    return (
+        <div className="flex flex-col">
+            <Link href='https://preeminent-banoffee-25a090.netlify.app/' target='_blank'>Burgers</Link>
+            <Link href='https://teal-shortbread-702f8c.netlify.app/' target="_blank">Nilambur</Link>
+            <Link href='https://timely-lollipop-fb855f.netlify.app/' target="_blank">Chat</Link>
+            <Link href='https://transcendent-sopapillas-e2eaee.netlify.app/' target="_blank">Unit Converter</Link>
+            <Link href='https://euphonious-bavarois-1e7981.netlify.app/' target="_blank">Password Generator</Link>
+            <Link href='https://jocular-sawine-04082b.netlify.app/' target="_blank">Scoreboard</Link>
+            <Link href='https://dainty-blini-640110.netlify.app/login' target="_blank">Funding Dashboard</Link>
+            <Link href='https://gtdlast-git-master-abelkjohns-projects.vercel.app' target="_blank">Getting Things Done</Link>
+        </div>
+    )
+}
---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "./Components/header"
+import Footer from "./Components/Footer"
+import WebLinks from "./Components/Websites"
+
+export default function Home() {
+  return <div>
+    <Header />
+    <WebLinks />
+    <Footer />
+    </div>
+}
