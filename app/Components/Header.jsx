c152865317ea41c8f20dfd04e57ed6df47088fdf
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-02 14:58:10 +0530

    modified:   app/Components/Header.jsx 	modified:   app/Components/Websites.jsx 	modified:   app/globals.css 	new file:   app/images/Me.jpg 	modified:   app/page.jsx

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import me from '../images/Me.jpg'
 
 export default function Header(){
-    return <h1>Header</h1>
+    return (
+        <div className="flex w-full py-2 justify-around items-center shadow-indigo-100 shadow-lg">
+            <img className="w-20 rounded-full" src={me.src} alt='pic' />
+            <h1 className="text-2xl">helloWorld.</h1>
+            <ul className="flex gap-4">
+                <li>About</li>
+                <li>Websites</li>
+                <li>Contacts</li>
+            </ul>
+        </div>
+    )
 }
--- app/Components/Websites.jsx
+++ app/Components/Websites.jsx
@@ -8,7 +8,6 @@ import pass from '../images/GenPass.jpeg'
 import gtd from '../images/GTD.jpeg'
 import converter from '../images/Converter.jpeg'
 import scoreboard from '../images/Scoreboard.jpeg'
-console.log(burger)
 
 
 export default function WebLinks(){
@@ -18,7 +17,7 @@ export default function WebLinks(){
             <Link className="ind-web" href='https://preeminent-banoffee-25a090.netlify.app/'target='_blank'><img src={burger.src} /></Link>
             <Link className="ind-web" href='https://euphonious-bavarois-1e7981.netlify.app/' target="_blank"><img src={pass.src} /></Link>
             <Link className="ind-web" href='https://jocular-sawine-04082b.netlify.app/' target="_blank"><img src={scoreboard.src} /></Link>
-            <Link className="ind-web" href='https://transcendent-sopapillas-e2eaee.netlify.app/' target="_blank"><img src={converter.src} /></Link>
+            <Link className="ind-web bg-gray-100" href='https://transcendent-sopapillas-e2eaee.netlify.app/' target="_blank"><img src={converter.src} /></Link>
             <Link className="ind-web" href='https://timely-lollipop-fb855f.netlify.app/' target="_blank"><img src={chats.src} /></Link>
             <Link className="ind-web" href='https://dainty-blini-640110.netlify.app/login' target="_blank"><img src={funding.src} /></Link>
             <Link className="ind-web" href='https://gtdlast-git-master-abelkjohns-projects.vercel.app' target="_blank"><img src={gtd.src} /></Link>
