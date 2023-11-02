c888830259a2220987f9573b65bf88e83aeeaa86
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-02 22:12:32 +0530

    modified:   app/Components/Header.jsx

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -5,8 +5,8 @@ export default function Header(){
     return (
         <div className=" flex w-full py-2 justify-around items-center shadow-indigo-100 shadow-lg">
             <img className="w-10 md:w-20 rounded-full" src={me.src} alt='pic' />
-            <h1 className=" text-lg md:text-3xl  ">helloWorld.</h1>
-            <ul className="flex gap-4 text-m md:text-lg">
+            <h1 className=" text-lg md:text-3xl">helloWorld.</h1>
+            <ul className="flex gap-2 md:gap-4 text-s md:text-lg">
                 <li>About</li>
                 <li>Websites</li>
                 <li>Contacts</li>
