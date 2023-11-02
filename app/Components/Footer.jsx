22a07ead724c8cbc1e3ea689c94915f39b813476
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-02 21:34:02 +0530

    modified:   app/Components/Footer.jsx 	modified:   package-lock.json 	modified:   package.json

--- app/Components/Footer.jsx
+++ app/Components/Footer.jsx
@@ -1,5 +1,18 @@
+
+
 import React from "react";
+import Link from 'next/link'
+import { FaInstagram, FaGit, FaFacebook, FaLinkedin } from 'react-icons/fa'
 
 export default function Footer(){
-    return <h1>Footer</h1>
+
+
+    return (
+        <div className="flex gap-10 justify-center text-3xl m-4">
+            <Link href="https://github.com/abelkjohn" target="_blank"><FaGit /></Link>
+            <Link href="https://www.facebook.com/adona.john.10" target="_blank"><FaFacebook /></Link>
+            <Link href="https://www.instagram.com/abel_kjohn/" target="_blank"><FaInstagram /></Link>
+            <Link href="https://www.linkedin.com/in/abel-john-585a831b0/" target="_blank"><FaLinkedin /></Link>
+        </div>
+    )
 }
