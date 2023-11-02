ef73033594541e81ec43f754585f2d8213f8841c
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-02 22:59:30 +0530

    modified:   app/Components/Websites.jsx

--- app/Components/Websites.jsx
+++ app/Components/Websites.jsx
@@ -14,9 +14,9 @@ export default function WebLinks(){
     return (
         <div className="flex flex-wrap gap-6 my-8 mx-auto w-11/12 justify-center">
             <Link className="ind-web" href='https://teal-shortbread-702f8c.netlify.app/' target="_blank"><img src={nilambur.src} /></Link>
-            <Link className="ind-web" href='https://preeminent-banoffee-25a090.netlify.app/'target='_blank'><img src={burger.src} /></Link>
-            <Link className="ind-web" href='https://euphonious-bavarois-1e7981.netlify.app/' target="_blank"><img src={pass.src} /></Link>
-            <Link className="ind-web" href='https://jocular-sawine-04082b.netlify.app/' target="_blank"><img src={scoreboard.src} /></Link>
+            <Link className="ind-web" href='https://6543d93bd052e22381dc3020--vermillion-kitten-ebe710.netlify.app/'target='_blank'><img src={burger.src} /></Link>
+            <Link className="ind-web" href='https://6543dafbe0e90d1e3276f639--dainty-pika-ccf3b3.netlify.app/' target="_blank"><img src={pass.src} /></Link>
+            <Link className="ind-web" href='https://6543dc07c8cb3a2b2ad7b5a0--dreamy-licorice-4a4e96.netlify.app/' target="_blank"><img src={scoreboard.src} /></Link>
             <Link className="ind-web bg-gray-100" href='https://transcendent-sopapillas-e2eaee.netlify.app/' target="_blank"><img src={converter.src} /></Link>
             <Link className="ind-web" href='https://timely-lollipop-fb855f.netlify.app/' target="_blank"><img src={chats.src} /></Link>
             <Link className="ind-web" href='https://dainty-blini-640110.netlify.app/login' target="_blank"><img src={funding.src} /></Link>
