ee5ddd4287da4e01fd1b416f61791eed8cfbae38
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-03 12:51:31 +0530

    modified:   app/Components/About.jsx 	modified:   app/Components/Header.jsx 	modified:   app/Components/Websites.jsx

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import me from '../images/Me.jpg'
-import Image from "next/image";
 
 export default function Header(){
     return (
         <div className="bg-white  fixed left-0 top-0 flex w-full py-2 justify-around items-center shadow-indigo-100 shadow-lg">
-            <Image className="w-10 md:w-20 rounded-full" src={me.src} alt='pic' />
+            <img className="w-10 md:w-20 rounded-full" src={me.src} alt='pic' />
             <h1 className=" text-lg md:text-3xl">helloWorld.</h1>
             <ul className="flex gap-2 text-sm md:gap-4 md:text-lg">
                 <li>About</li>
--- app/Components/Websites.jsx
+++ app/Components/Websites.jsx
@@ -8,20 +8,19 @@ import pass from '../images/GenPass.jpeg'
 import gtd from '../images/GTD.jpeg'
 import converter from '../images/Converter.jpeg'
 import scoreboard from '../images/Scoreboard.jpeg'
-import Image from "next/image";
 
 
 export default function WebLinks(){
     return (
         <div className="flex flex-wrap gap-6 my-8 mx-auto w-11/12 justify-center">
-            <Link className="ind-web" href='https://teal-shortbread-702f8c.netlify.app/' target="_blank"><Image src={nilambur.src} /></Link>
-            <Link className="ind-web" href='https://6543d93bd052e22381dc3020--vermillion-kitten-ebe710.netlify.app/'target='_blank'><Image src={burger.src} /></Link>
-            <Link className="ind-web" href='https://6543dafbe0e90d1e3276f639--dainty-pika-ccf3b3.netlify.app/' target="_blank"><Image src={pass.src} /></Link>
-            <Link className="ind-web" href='https://6543dc07c8cb3a2b2ad7b5a0--dreamy-licorice-4a4e96.netlify.app/' target="_blank"><Image src={scoreboard.src} /></Link>
-            <Link className="ind-web bg-gray-100" href='https://6543dfdb8452f2347c61cbcb--deluxe-cranachan-f3c317.netlify.app/' target="_blank"><Image src={converter.src} /></Link>
-            <Link className="ind-web" href='https://timely-lollipop-fb855f.netlify.app/' target="_blank"><Image src={chats.src} /></Link>
-            <Link className="ind-web" href='https://dainty-blini-640110.netlify.app/login' target="_blank"><Image src={funding.src} /></Link>
-            <Link className="ind-web" href='https://gtdlast-git-master-abelkjohns-projects.vercel.app' target="_blank"><Image src={gtd.src} /></Link>
+            <Link className="ind-web" href='https://teal-shortbread-702f8c.netlify.app/' target="_blank"><img src={nilambur.src} /></Link>
+            <Link className="ind-web" href='https://6543d93bd052e22381dc3020--vermillion-kitten-ebe710.netlify.app/'target='_blank'><img src={burger.src} /></Link>
+            <Link className="ind-web" href='https://6543dafbe0e90d1e3276f639--dainty-pika-ccf3b3.netlify.app/' target="_blank"><img src={pass.src} /></Link>
+            <Link className="ind-web" href='https://6543dc07c8cb3a2b2ad7b5a0--dreamy-licorice-4a4e96.netlify.app/' target="_blank"><img src={scoreboard.src} /></Link>
+            <Link className="ind-web bg-gray-100" href='https://6543dfdb8452f2347c61cbcb--deluxe-cranachan-f3c317.netlify.app/' target="_blank"><img src={converter.src} /></Link>
+            <Link className="ind-web" href='https://timely-lollipop-fb855f.netlify.app/' target="_blank"><img src={chats.src} /></Link>
+            <Link className="ind-web" href='https://dainty-blini-640110.netlify.app/login' target="_blank"><img src={funding.src} /></Link>
+            <Link className="ind-web" href='https://gtdlast-git-master-abelkjohns-projects.vercel.app' target="_blank"><img src={gtd.src} /></Link>
             
         </div>
     )
