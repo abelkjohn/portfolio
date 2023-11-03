03af0cae57539c3b5176f063407f1f0568e8a106
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-03 12:40:13 +0530

    modified:   app/Components/About.jsx     modified:   app/Components/Header.jsx     modified:   app/Components/Websites.jsx

--- app/Components/About.jsx
+++ app/Components/About.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Me from '../images/Me.jpg'
+import Image from "next/image";
 
 export default function About(){
     return <div className="flex flex-col md:flex-row mx-3 gap-4 md:m-32 text-center md:text-left">
-        <img className="rounded-full h-min" src={Me.src} />
+        <Image className="rounded-full h-min" src={Me.src} alt="Profile pic"/>
         <div className="flex flex-col justify-center">
             <h1 className="text-lg">About Abel K John</h1>
             <p className="mx-3 mb-5">I'm Abel K John, a budding React.js front-end developer. Proficient in HTML, CSS, and JavaScript, I specialize in crafting user-friendly web experiences. My projects are born from a desire to meet unique needs, showcasing a blend of creativity and technical skill.</p>
