026c89dace46676063ee596ff8e57c2e08c5ee6b
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-08 19:06:10 +0530

    modified:   app/Components/About.jsx 	modified:   app/Components/Skills.jsx

--- app/Components/Skills.jsx
+++ app/Components/Skills.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function Skills(){
     return (
-        <div className="flex w-11/12 justify-end mx-auto gap-6">
-            <div className="w-1/3">
+        <div className="flex w-11/12 justify-end mx-auto gap-6 flex-wrap lg:flex-nowrap">
+            <div className="pt-4 md:w-1/3 lg:w-1/3">
                 <h1>Get to know me:</h1>
-                <div>My background is in Business Administration and Marketing/Sales . I&apos;am currently working on a bachelors degree in Business Administration from Amity University Online. When I&apos;m not coding, I play the guitar or explore.
+                <div className="p-2 pt-1">My background is in Business Administration and Marketing/Sales . I&apos;am currently working on a bachelors degree in Business Administration from Amity University Online. When I&apos;m not coding, I play the guitar or explore.
             </div>
             </div>
             <div className="flex flex-col justify-start w-96 my-4 ">
-            <div>My Technical Skills:</div>
+            <div className="mb-1">My Technical Skills:</div>
             <div className="flex gap-2 flex-wrap">
                 <div className="skill-button">HTML5</div>
                 <div className="skill-button">CSS3</div>
@@ -23,7 +23,7 @@ export default function Skills(){
             </div>       
         </div>
         <div className="flex flex-col justify-start w-96 my-4 ">
-            <div>My Social Skills:</div>
+            <div className="mb-1">My Social Skills:</div>
             <div className="flex gap-2 flex-wrap">
                 <div className="skill-button">Servant Leadership</div>
                 <div className="skill-button">Active Listening</div>
