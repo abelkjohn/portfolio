6534871902bf942c0070dd1d9535ff569c18ce49
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-08 11:16:27 +0530

    modified:   app/Components/About.jsx 	modified:   app/Components/Header.jsx 	modified:   app/layout.js

--- app/Components/Header.jsx
+++ app/Components/Header.jsx
@@ -5,9 +5,9 @@ export default function Header(){
     return (
         <div className="bg-white  fixed left-0 top-0 flex w-full py-2 justify-around items-center shadow-indigo-100 shadow-lg">
             <img className="w-10 lg:w-20 rounded-full" src={me.src} alt='pic' />
-            <h1 className="text-lg md:text-3xl">helloWorld.</h1>
+            <p className=" text-lg md:text-3xl">helloWorld.</p>
             <ul className="flex gap-2 text-sm md:gap-4 md:text-lg">
-                <li><a href="#about">About</a></li>
+                <li className="hover:underline"><a href="#about">About</a></li>
                 <li><a href="#websites">Websites</a></li>
                 <li><a href="#contacts">Contact</a></li>
             </ul>
