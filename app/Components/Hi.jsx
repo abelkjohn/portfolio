4ff4435ff693ef085b22e6ee2d150c2de0c36b0e
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-08 23:30:38 +0530

    modified:   app/Components/Hi.jsx 	modified:   app/globals.css

--- app/Components/Hi.jsx
+++ app/Components/Hi.jsx
@@ -6,9 +6,9 @@ export default function Hi(){
         <div className=" relative h-96 flex justify-center items-center ">
             <img className="absolute top-10 custom-right custom-me-img md:w-60 lg:w-60 shadow-2xl shadow-indigo-500/50" src={Me.src}></img>
             <div className="flex flex-col items-end">
-                <p className="text-2xl w-1/2 text-left pl-3">Hi, I am</p>
-                <p className="text-2xl w-1/2 text-left pl-3"><strong>Abel K John</strong></p>
-                <p className="text-xl p-2 bg-indigo-200 custom-width text-right">Front-End Developer</p>
+                <p className="text-2xl pr-28 md:w-1/2 lg:w-1/2 text-left pl-3 custom-hi">Hi, I am</p>
+                <p className="text-2xl pr-12 md:w-1/2 lg:w-1/2 text-left pl-3 custom-name"><strong>Abel K John</strong></p>
+                <p className="text-lg md:text-xl lg:text-xl p-2 bg-indigo-200 custom-width text-right">Front-End Developer</p>
             </div>
         </div>
     )
