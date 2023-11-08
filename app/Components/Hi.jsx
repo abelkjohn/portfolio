ad10dee62df259ec54759372634fae2949d1794b
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-08 23:15:41 +0530

    modified:   app/Components/Hi.jsx 	modified:   app/globals.css

--- app/Components/Hi.jsx
+++ app/Components/Hi.jsx
@@ -4,7 +4,7 @@ import Me from '../images/Me2.jpg'
 export default function Hi(){
     return (
         <div className=" relative h-96 flex justify-center items-center ">
-            <img className="absolute top-10 custom-right w-60 shadow-2xl shadow-indigo-500/50" src={Me.src}></img>
+            <img className="absolute top-10 custom-right custom-me-img md:w-60 lg:w-60 shadow-2xl shadow-indigo-500/50" src={Me.src}></img>
             <div className="flex flex-col items-end">
                 <p className="text-2xl w-1/2 text-left pl-3">Hi, I am</p>
                 <p className="text-2xl w-1/2 text-left pl-3"><strong>Abel K John</strong></p>
