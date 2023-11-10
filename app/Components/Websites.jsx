118d65a85f4cf153d6bb6beaa4924346b4497e8c
abelkjohn <126896178+abelkjohn@users.noreply.github.com>
2023-11-11 00:57:32 +0530

    modified:   app/Components/Websites.jsx 	renamed:    app/[id]/page.tsx -> app/websites/[id]/page.tsx

--- app/Components/Websites.jsx
+++ app/Components/Websites.jsx
@@ -19,13 +19,14 @@ const getProjects = async() => {
 
 export default async function WebLinks(){
     
-    const { projects } = await getProjects()
+    const projects = await getProjects()
+    console.log(projects.projects)
     return (
         <div id="websites" className="flex flex-col items-center ">
             <h1 className="text-3xl mt-28 md:mt-32 lg:32">My Projects</h1>
             <div  className="flex flex-wrap gap-6 my-8 mx-auto w-11/12 justify-center ">
-            {projects.map(i => {
-                const link = `/${i._id}`
+            {projects.projects.map(i => {
+                const link = `/websites/${i._id}`
                  return <Link key={i._id} className="ind-web" href={link}><img src={i.image} /></Link>
             })}
 
